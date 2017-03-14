a5d51d254395e7f04aef421811c22379382178ca
Julio Cesar Quispe <julio.quispe@jalasoft.com>
2017-03-14 09:41:14 -0400

    version2

---
+++ src/client/app.module.js
@@ -0,0 +1,32 @@
+/* this is the model file for Team module*/
+import angular;
+import navbar from '/components/navbar/navbarModule';
+
+export const championshipApp = angular.module("championshipApp", [
+	"ngRoute",
+	navbar
+]);
+
+championshipApp.config($routeProvider => {
+    $routeProvider
+    .when('/public', {
+        templateUrl : '/components/navbar/navbar.html',
+        controller: '/components/navbar/navbarController'
+    });
+    /*.when("/team", {
+        templateUrl : "team.htm"
+    })
+    .when("/championship", {
+        templateUrl : "championship.htm"
+    });*/
+});
+
+	
+let appModule = angular.module('championshipApp', [
+
+]);
+
+appModule.controller('AppController', AppController);
+
+export default appModule;
+    
