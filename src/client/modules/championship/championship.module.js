944ef689e8162db781a21026acc292aa97a37f18
Julio Cesar Quispe <julio.quispe@jalasoft.com>
2017-05-08 18:36:36 -0400

    permission

--- src/client/modules/championship/championship.module.js
+++ src/client/modules/championship/championship.module.js
@@ -1,17 +1,11 @@
 
 
-import TeamController from './controllers/championship.controller';
-import CreateTeamController from './team/create/create-team.controller';
 import ClubController from './club/create/create-club.controller';
 import routertConfig from './championship.routes';
 let championshipModule = angular.module('championship', [
 
 ]);
-
 championshipModule.config(routertConfig);
-//championshipModule.controller('ClubController', ClubController);
-championshipModule.controller('TeamController', TeamController);
-championshipModule.controller('CreateTeamController', CreateTeamController);
 championshipModule.controller('ClubController', ClubController);
 
 export default championshipModule;
